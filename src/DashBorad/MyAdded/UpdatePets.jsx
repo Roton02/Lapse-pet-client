e143e336781ba811742869c55bc00d5585020020
Roton02 <rph645102@gmail.com>
2024-06-23 00:05:03 +0600

    resolved and all complete

--- src/DashBorad/MyAdded/UpdatePets.jsx
+++ src/DashBorad/MyAdded/UpdatePets.jsx
@@ -1,65 +1,65 @@
 import { useEffect, useState } from "react";
 import Select from "react-select";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { BiVerticalBottom } from "react-icons/bi";
 import useAuth from "../../Hooks/useAuth";
 import { imageUpload } from "../../api/utils";
-import { useLoaderData, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Swal from "sweetalert2";
 import useAxiosSecure from "../../Hooks/useAxiosSecure";
 import { Helmet } from "react-helmet-async";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const UpdatePets = () => {
-  const [ prevData ,setPrevData  ] = useState({})
-  const params = useParams()
-  console.log(params);
+  const [prevData, setPrevData] = useState({});
+  const params = useParams();
   const navigate = useNavigate();
   const axiosSecure = useAxiosSecure();
-  // console.log(loadedData);
-  useEffect(()=> {
-    axiosSecure(`https://lapsepets.vercel.app/myAdded/?id=${params.id}`)
-    .then(res => {
-     setPrevData(res.data[0] )
-    })
-  },[axiosSecure])
-   console.log(prevData);
-  const { _id, name, age, type, img, description, description2, location } =
-    prevData || {};
-  const {
-    register,
-    reset,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+
+  useEffect(() => {
+    axiosSecure(`https://lapsepets.vercel.app/myAdded/?id=${params.id}`).then((res) => {
+      setPrevData(res.data[0]);
+    });
+  }, [axiosSecure, params.id]);
+
+  const { _id, name, age, type, img, description, description2, location } = prevData || {};
+  const { register, reset, handleSubmit, formState: { errors }, control } = useForm();
   const { user } = useAuth();
   const [selectedOption, setSelectedOption] = useState(type);
+
   useEffect(() => {
-    reset();
-  }, [reset]);
-  useEffect(() => {
-    reset();
-  }, [reset]);
+    reset({
+      name,
+      age,
+      type,
+      location,
+      note1: description,
+      note2: description2
+    });
+  }, [reset, name, age, type, location, description, description2]);
+
   const onSubmit = async (data) => {
     let newImage = img;
     if (data.photo.length) {
       try {
         const imgData = await imageUpload(data.photo[0]);
         newImage = imgData;
-        console.log(imgData);
       } catch (err) {
-        console.log(err);
+        console.error(err);
       }
     }
+
     const petDetails = {
       name: data.name || name,
       type: selectedOption || type,
-      img: newImage ,
+      img: newImage,
       location: data.location || location,
       age: data.age || age,
       description: data.note1 || description,
       description2: data.note2 || description2,
     };
-    console.log(petDetails);
+
     axiosSecure.patch(`updateMyaddedPets/${_id}`, petDetails).then((res) => {
       if (res.data.modifiedCount > 0) {
         Swal.fire({
@@ -82,179 +82,153 @@ const UpdatePets = () => {
 
   const handleChange = (selectedOption) => {
     setSelectedOption(selectedOption.value);
-    console.log(`Option selected:`, selectedOption);
-    // setPetType(selectedOption)
   };
 
-  useEffect(()=>{
-    const id = document.getElementById('text')
-    id.innerHTML =prevData.description2
-
-  },[prevData.description2])
-
-
   return (
-   
     <div>
-       <Helmet>
+      <Helmet>
         <title>Lapse-Peat || Update My Added Peats</title>
-        {/* <link rel="canonical" href="https://www.tacobell.com/" /> */}
       </Helmet>
       <div className="relative">
         <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
-          <div className="">
-            <div>
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="lg:max-w-lg lg:mx-auto  ms-auto">
-                  <div className="p-4 sm:p-7 flex flex-col bg-white rounded-2xl shadow-lg dark:bg-neutral-900">
-                    <div className="text-center">
-                      <h1
-                        className="block text-2xl font-bold text-gray-800
-                 dark:text-white"
-                      >
-                        Update peats
-                      </h1>
+          <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="lg:max-w-lg lg:mx-auto ms-auto">
+                <div className="p-4 sm:p-7 flex flex-col bg-white rounded-2xl shadow-lg dark:bg-neutral-900">
+                  <div className="text-center">
+                    <h1 className="block text-2xl font-bold text-gray-800 dark:text-white">Update peats</h1>
+                  </div>
+
+                  <div className="mt-5">
+                    <div className="py-3 flex items-center text-xs text-gray-400 uppercase before:flex-1 before:border-t before:border-gray-200 before:me-6 after:flex-1 after:border-t after:border-gray-200 after:ms-6 dark:text-neutral-500 dark:before:border-neutral-700 dark:after:border-neutral-700">
+                      <BiVerticalBottom />
                     </div>
-
-                    <div className="mt-5">
-                      <div className="py-3 flex items-center text-xs text-gray-400 uppercase before:flex-1 before:border-t before:border-gray-200 before:me-6 after:flex-1 after:border-t after:border-gray-200 after:ms-6 dark:text-neutral-500 dark:before:border-neutral-700 dark:after:border-neutral-700">
-                        <BiVerticalBottom />
-                      </div>
-                      <div className="grid grid-cols-2 gap-4">
-                        <div className="form-control">
-                          <label className="label">
-                            <span className="label-text">Name</span>
-                          </label>
-                          <input
-                            type="text"
-                            {...register("name", )}
-                            defaultValue={name}
-                            name="name"
-                            placeholder="Name"
-                            className="input input-bordered"
-                          />
-                          {errors.name && (
-                            <span className="text-red-600">
-                              Name is required
-                            </span>
-                          )}
-                        </div>
-                        <div className="form-control">
-                          <label className="label">
-                            <span className="label-text">Age</span>
-                          </label>
-                          <input
-                            type="text"
-                            {...register("age", )}
-                            defaultValue={age}
-                            name="age"
-                            placeholder="peat age"
-                            className="input input-bordered"
-                          />
-                          {errors.age && (
-                            <span className="text-red-600">
-                              Age is required
-                            </span>
-                          )}
-                        </div>
-                        <div className="form-control">
-                          <label className="label">
-                            <span className="label-text">Image</span>
-                          </label>
-                          <input
-                            type="file"
-                            {...register("photo",)}
-                            // defaultValue={img}
-                            name="photo"
-                            placeholder="Photo"
-                            className="input "
-                          />
-                          {errors.photo && (
-                            <span className="text-red-600">
-                              Image is required
-                            </span>
-                          )}
-                        </div>
-                        <div className="form-control">
-                          <label className="label">
-                            <span className="label-text">Type</span>
-                          </label>
-                          <Select
-                            value={selectedOption}
-                            onChange={handleChange}
-                            options={options}
-                            placeholder={selectedOption}
-                          />
-                        </div>
-                      </div>
+                    <div className="grid grid-cols-2 gap-4">
                       <div className="form-control">
                         <label className="label">
-                          <span className="label-text">location</span>
+                          <span className="label-text">Name</span>
                         </label>
                         <input
                           type="text"
-                          {...register("location", )}
-                          defaultValue={location}
-                          name="location"
-                          placeholder="write reciver location"
+                          {...register("name")}
+                          defaultValue={name}
+                          name="name"
+                          placeholder="Name"
                           className="input input-bordered"
                         />
-                        {errors.location && (
-                          <span className="text-red-600">
-                            Location is required
-                          </span>
-                        )}
+                        {errors.name && <span className="text-red-600">Name is required</span>}
                       </div>
                       <div className="form-control">
                         <label className="label">
-                          <span className="label-text">Short Note</span>
+                          <span className="label-text">Age</span>
                         </label>
                         <input
                           type="text"
-                          {...register("note1", )}
-                          defaultValue={description}
-                          name="note1"
-                          placeholder="Note About Peat"
+                          {...register("age")}
+                          defaultValue={age}
+                          name="age"
+                          placeholder="peat age"
                           className="input input-bordered"
                         />
-                        {errors.note1 && (
-                          <span className="text-red-600">Note is required</span>
-                        )}
+                        {errors.age && <span className="text-red-600">Age is required</span>}
                       </div>
                       <div className="form-control">
                         <label className="label">
-                          <span className="label-text">Description</span>
+                          <span className="label-text">Image</span>
                         </label>
-                        <textarea
-                        id="text"
-                          {...register("note2", )}
-                          defaultValue={description2}
-                          className="textarea textarea-secondary"
-                          placeholder="Write Above Peats"
-                        ></textarea>
-
-                        {errors.note1 && (
-                          <span className="text-red-600">Note is required</span>
+                        <input
+                          type="file"
+                          {...register("photo")}
+                          name="photo"
+                          placeholder="Photo"
+                          className="input"
+                        />
+                        {errors.photo && <span className="text-red-600">Image is required</span>}
+                      </div>
+                      <div className="form-control">
+                        <label className="label">
+                          <span className="label-text">Type</span>
+                        </label>
+                        <Controller
+                          name="type"
+                          control={control}
+                          defaultValue={selectedOption}
+                          render={({ field }) => (
+                            <Select
+                              {...field}
+                              value={options.find(option => option.value === field.value) || options.find(option => option.value === type)}
+                              onChange={(val) => {
+                                field.onChange(val.value);
+                                handleChange(val);
+                              }}
+                              options={options}
+                              placeholder="Select pet type"
+                            />
+                          )}
+                        />
+                      </div>
+                    </div>
+                    <div className="form-control">
+                      <label className="label">
+                        <span className="label-text">Location</span>
+                      </label>
+                      <input
+                        type="text"
+                        {...register("location")}
+                        defaultValue={location}
+                        name="location"
+                        placeholder="write receiver location"
+                        className="input input-bordered"
+                      />
+                      {errors.location && <span className="text-red-600">Location is required</span>}
+                    </div>
+                    <div className="form-control">
+                      <label className="label">
+                        <span className="label-text">Short Note</span>
+                      </label>
+                      <input
+                        type="text"
+                        {...register("note1")}
+                        defaultValue={description}
+                        name="note1"
+                        placeholder="Note About Peat"
+                        className="input input-bordered"
+                      />
+                      {errors.note1 && <span className="text-red-600">Note is required</span>}
+                    </div>
+                    <div className="form-control">
+                      <label className="label">
+                        <span className="label-text">Description</span>
+                      </label>
+                      <Controller
+                        name="note2"
+                        control={control}
+                        defaultValue={description2}
+                        render={({ field }) => (
+                          <ReactQuill
+                            {...field}
+                            theme="snow"
+                            value={field.value}
+                            onChange={field.onChange}
+                            placeholder="Write About Peat"
+                          />
                         )}
-                      </div>
-
-                      <div className="mt-5 flex justify-center ">
-                        <button
-                          type="submit"
-                          value={""}
-                          className="rounded-md w-1/3  btn  overflow-hidden relative group cursor-pointer border-2  font-medium border-[#ff4880] text-[#1e847f]hover:text-white"
-                        >
-                          <span className="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-[#ff4880] top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
-                          <span className="relative my-auto  text-[#ff4880] transition duration-300 group-hover:text-white ease">
-                            Update
-                          </span>
-                        </button>
-                      </div>
+                      />
+                      {errors.note2 && <span className="text-red-600">Description is required</span>}
+                    </div>
+                    <div className="mt-5 flex justify-center">
+                      <button
+                        type="submit"
+                        className="rounded-md w-1/3 btn overflow-hidden relative group cursor-pointer border-2 font-medium border-[#ff4880] text-[#1e847f] hover:text-white"
+                      >
+                        <span className="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-[#ff4880] top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
+                        <span className="relative my-auto text-[#ff4880] transition duration-300 group-hover:text-white ease">Update</span>
+                      </button>
                     </div>
                   </div>
                 </div>
-              </form>
-            </div>
+              </div>
+            </form>
           </div>
         </div>
       </div>
