7c123980fce246d4a0b29a9f2ad55fa4777ca2dd
Roton02 <rph645102@gmail.com>
2024-07-15 21:46:10 +0600

    Fixed: fixed navbar for learge and small device

--- src/Shared/Navbar/Navbar2.jsx
+++ src/Shared/Navbar/Navbar2.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
+
 const Navbar2 = () => {
-  const [isOpen, setIsopen] = false;
+  const [isOpen, setIsopen] = useState(false);
   return (
     <div>
       <nav
+        // x-data="{ isOpen: false }"
         className="relative bg-white shadow dark:bg-gray-800"
       >
         <div className="container px-6 py-4 mx-auto">
@@ -17,36 +20,50 @@ const Navbar2 = () => {
               </a>
               <div className="flex lg:hidden">
                 <button
+                  onClick={() => setIsopen(!isOpen)}
+                  // x-cloak @click="isOpen = !isOpen"
                   type="button"
                   className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400"
                   aria-label="toggle menu"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className={`w-6 h-6 hidden:${isOpen}`}
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path d="M4 8h16M4 16h16" />
-                  </svg>
-
-                  <svg
-                    x-show="isOpen"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path d="M6 18L18 6M6 6l12 12" />
-                  </svg>
+                  {isOpen ? (
+                    <>
+                      {" "}
+                      <svg
+                        // x-show="isOpen"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-6 h-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path d="M6 18L18 6M6 6l12 12" />
+                      </svg>{" "}
+                    </>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className={`w-6 h-6`}
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path d="M4 8h16M4 16h16" />
+                    </svg>
+                  )}
                 </button>
               </div>
             </div>
 
             <div className="absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white dark:bg-gray-800 lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:bg-transparent lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center">
-              <div className="flex flex-col -mx-6 lg:flex-row lg:items-center lg:mx-8">
+              <div
+                // x-cloak:class="[isOpen ? 'translate-x-0 opacity-100 ' : 'opacity-0 -translate-x-full']"
+                className={`flex flex-col -mx-6 lg:flex-row lg:items-center lg:mx-8 ${
+                  isOpen
+                    ? "translate-x-0 opacity-100 block"
+                    : "opacity-0 -translate-x-full hidden"
+                }`}
+              >
                 <a
                   href="#"
                   className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
@@ -73,9 +90,9 @@ const Navbar2 = () => {
                 </a>
               </div>
 
-              <div className="flex items-center mt-4 lg:mt-0">
+              <div className={` ${isOpen ? "block": "hidden"} flex items-center mt-4 lg:mt-0`}>
                 <button
-                  className="hidden mx-4 text-gray-600 transition-colors duration-300 transform lg:block dark:text-gray-200 hover:text-gray-700 dark:hover:text-gray-400 focus:text-gray-700 dark:focus:text-gray-400 focus:outline-none"
+                  className={` ${isOpen ? "block": "hidden"} mx-4 text-gray-600 transition-colors duration-300 transform lg:block dark:text-gray-200 hover:text-gray-700 dark:hover:text-gray-400 focus:text-gray-700 dark:focus:text-gray-400 focus:outline-none`}
                   aria-label="show notifications"
                 >
                   <svg
