7f1d055800bde3a0262b7d66a6c69aa0d38f93d2
Roton02 <rph645102@gmail.com>
2024-08-10 10:34:11 +0600

    Update: update the header . header anchor change and active the link in other tabs

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -1,5 +1,5 @@
 import { BsFillTelephonePlusFill } from "react-icons/bs";
-import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+import { FaFacebook, FaInstagram, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 import { IoMdMail } from "react-icons/io";
 import { IoLocation } from "react-icons/io5";
 
@@ -27,15 +27,15 @@ const Header = () => {
           </p>
         </div>
         <div className="flex gap-8">
-          <p className=" p-2">
+          <a href="https://web.facebook.com/roton.chodiry" target="_blank" className=" p-2">
             <FaFacebook />
-          </p>
-          <p className=" p-2">
-            <FaInstagram></FaInstagram>
-          </p>
-          <p className=" p-2">
+          </a>
+          <a href="https://web.facebook.com/roton.chodiry" target="_blank" className=" p-2">
+          <FaLinkedinIn></FaLinkedinIn>
+          </a>
+          <a href="https://x.com/MdSanaulla75762" target="_blank" className=" p-2">
             <FaTwitter></FaTwitter>
-          </p>
+          </a>
         </div>
       </div>
     </div>
