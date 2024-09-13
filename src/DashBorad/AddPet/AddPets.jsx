c131fc512c52da8735cf2f2860939fba0e26b01f
Roton02 <rph645102@gmail.com>
2024-09-13 17:11:25 +0600

    update the design in create campaign and add a pet page

--- src/DashBorad/AddPet/AddPets.jsx
+++ src/DashBorad/AddPet/AddPets.jsx
@@ -115,7 +115,7 @@ const AddPets = () => {
                     {...register("name", { required: true })}
                     name="name"
                     placeholder="Name"
-                    className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none sm:text-sm"
+                    className="mt-1 block w-full px-3 py-2 bg-white  border rounded-md border-secondary"
                   />
                   {errors.name && (
                     <span className="text-red-600 text-sm">
@@ -130,7 +130,7 @@ const AddPets = () => {
                     {...register("age", { required: true })}
                     name="age"
                     placeholder="Pet age"
-                    className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none sm:text-sm"
+                    className="mt-1 block w-full px-3 py-2 bg-white  border rounded-md border-secondary"
                   />
                   {errors.age && (
                     <span className="text-red-600 text-sm">
@@ -145,7 +145,7 @@ const AddPets = () => {
                     {...register("photo", { required: true })}
                     name="photo"
                     placeholder="Photo"
-                    className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
+                    className="mt-1 block w-full px-3 py-2 bg-white   border rounded-md border-secondary"
                   />
                   {errors.photo && (
                     <span className="text-red-600 text-sm">
@@ -159,7 +159,7 @@ const AddPets = () => {
                     value={selectedOption}
                     onChange={handleChange}
                     options={options}
-                    className="mt-1"
+                    className="mt-2 p- border-none "
                   />
                 </div>
               </div>
@@ -170,7 +170,7 @@ const AddPets = () => {
                   {...register("location", { required: true })}
                   name="location"
                   placeholder="Location"
-                  className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none sm:text-sm"
+                  className="mt-1 block w-full px-3 py-2 bg-white  border rounded-md border-secondary"
                 />
                 {errors.location && (
                   <span className="text-red-600 text-sm">
@@ -185,7 +185,7 @@ const AddPets = () => {
                   {...register("note1", { required: true })}
                   name="note1"
                   placeholder="Short Note About Pet"
-                  className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none sm:text-sm"
+                  className="mt-1 block w-full px-3 py-2 bg-white  border rounded-md border-secondary"
                 />
                 {errors.note1 && (
                   <span className="text-red-600 text-sm">
