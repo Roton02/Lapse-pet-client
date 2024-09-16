a13deed2dd81cc2413366413b8c41d3b3f685527
Roton02 <rph645102@gmail.com>
2024-09-16 16:19:52 +0600

    clean the  all user page code

--- src/DashBorad/AllUsers/AllUsers.jsx
+++ src/DashBorad/AllUsers/AllUsers.jsx
@@ -1,5 +1,4 @@
 import { useQuery } from "@tanstack/react-query";
-import { FaBan } from "react-icons/fa";
 import Swal from "sweetalert2";
 import useAxiosSecure from "../../Hooks/useAxiosSecure";
 import { Helmet } from "react-helmet-async";
@@ -31,94 +30,12 @@ const AllUsers = () => {
     });
   };
 
-  const handleDeleteUser = (user) => {
-    Swal.fire({
-      title: "Are you sure?",
-      text: "You won't be able to revert this!",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, ban it!",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        axiosSecure.post("/users/ban", user).then((res) => {
-          if (res.data.insertedId) {
-            const remainingByBanned = users.filter((b) => b._id !== user._id);
-            console.log(remainingByBanned);
-            Swal.fire({
-              title: "Ban!",
-              text: "Your file has been Ban.",
-              icon: "success",
-            });
-          }
-        });
-      }
-    });
-  };
-
   return (
-    // <div className="-z-50 ">
-    //   <Helmet>
-    //     <title>Lapse-Peat || All Users</title>
-    //     {/* <link rel="canonical" href="https://www.tacobell.com/" /> */}
-    //   </Helmet>
-      // <div className="my-10">
-      //   <h2 className="text-4xl text-center">Total Users: {users.length}</h2>
-      // </div>
-    //   <div className=" ">
-    //     <table className="table  md:w-full w-[500px]">
-    //       {/* head */}
-    //       <thead>
-    //         <tr className="text-black space-x-2">
-    //           <th>No</th>
-    //           <th className="font-bold hidden md:inline-block">Profile</th>
-    //           <th className="font-bold hidden md:inline-block ml-0 md:ml-2">Name</th>
-    //           <th className="font-bold ">Email</th>
-    //           <th className="font-bold">Role</th>
-    //           <th className="font-bold">Delete</th>
-    //         </tr>
-    //       </thead>
-    // <tbody>
-    //   {users.map((user, index) => (
-    //     <tr key={user._id}>
-    //       <th>{index + 1}</th>
-    //       <td className="hidden md:inline-block">
-    //         <div className="avatar">
-    //           <div className="w-12 rounded-full">
-    //             <img src={user.image} />
-    //           </div>
-    //         </div>
-    //       </td>
-    //       <td className="hidden md:inline-block">{user.name}</td>
-    //       <td className="px-2 mx-0">{user.email}</td>
-    //       <td>
-    //         {user.role === "admin" ? (
-    //          <span className="btn btn-sm bg-green-500  px-4"> Admin</span>
-    //         ) : (
-    //           <button
-    //             onClick={() => handleMakeAdmin(user)}
-    //             className="btn btn-sm bg-pink-600 text-nowrap"
-    //           >
-    //             Make Admin
-    //           </button>
-    //         )}
-    //       </td>
-    //       <td>
-    //         <button
-    //           onClick={() => handleDeleteUser(user)}
-    //           className="btn btn-sm "
-    //         >
-    //           <FaBan className="text-red-700 text-2xl"></FaBan>
-    //         </button>
-    //       </td>
-    //     </tr>
-    //   ))}
-    // </tbody>
-    //     </table>
-    //   </div>
-    // </div>
     <div className="-z-50">
+      <Helmet>
+        <title>Lapse-Peat || All Users</title>
+        {/* <link rel="canonical" href="https://www.tacobell.com/" /> */}
+      </Helmet>
       <div className="my-10 -z-50">
         <h2 className="text-4xl text-center">Total Users: {users.length}</h2>
       </div>
@@ -139,7 +56,6 @@ const AllUsers = () => {
                       </th>
                       <th className="font-bold md:text-start ml-4">Email</th>
                       <th className="font-bold">Role</th>
-                      
                     </tr>
                   </thead>
 
@@ -154,7 +70,9 @@ const AllUsers = () => {
                             </div>
                           </div>
                         </td>
-                        <td className="hidden md:inline-block  ">{user.name}</td>
+                        <td className="hidden md:inline-block  ">
+                          {user.name}
+                        </td>
                         <td className="px-2 mx-0  ml-5 ">{user.email}</td>
                         <td className="mt-7 ">
                           {user.role === "admin" ? (
@@ -171,7 +89,6 @@ const AllUsers = () => {
                             </button>
                           )}
                         </td>
-                        
                       </tr>
                     ))}
                   </tbody>
