f8f7e474ee439f30e20d638a438c87b8cdec690c
Roton02 <rph645102@gmail.com>
2024-09-20 15:08:04 +0600

    implement and test AOS animation in  section title and call to action section also why chose us section

--- src/Page/Home/Choses.jsx
+++ src/Page/Home/Choses.jsx
@@ -10,32 +10,44 @@ const Choses = () => {
       <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-5">
         <div>
           <img
+            data-aos="fade-up"
+            data-aos-duration="2000"
             className="rounded-xl"
             src="https://kit.envalabdemos.com/peat/wp-content/uploads/2023/09/a-rescue-dog-is-at-the-veterinarian-on-a-leash-and-2021-09-01-15-09-41-utc.jpg "
             alt=""
           />
         </div>
         <img
+          data-aos="fade-up"
+          data-aos-duration="2000"
           className="rounded-xl"
           src="  https://kit.envalabdemos.com/peat/wp-content/uploads/2023/09/beautiful-woman-kissing-her-dog-at-sunset-2021-08-30-17-40-41-utc.jpg "
           alt=""
         />
         <img
+          data-aos="fade-up"
+          data-aos-duration="2000"
           className="rounded-xl"
           src="https://kit.envalabdemos.com/peat/wp-content/uploads/2023/09/cute-little-kittens-crying-in-carrier-box-two-hun-2021-09-02-10-55-28-utc-1.jpg  "
           alt=""
         />
         <img
+          data-aos="fade-up"
+          data-aos-duration="2000"
           className="rounded-xl"
           src=" https://kit.envalabdemos.com/peat/wp-content/uploads/2023/09/pet-love-homeless-dog-adoption-caring-for-a-pet-2021-08-30-22-00-55-utc-1.jpg  "
           alt=""
         />
         <img
+          data-aos="fade-up"
+          data-aos-duration="2000"
           className="rounded-xl"
           src="https://kit.envalabdemos.com/peat/wp-content/uploads/2023/09/national-pet-adoption-day-2021-08-29-01-05-31-utc.jpg   "
           alt=""
         />
         <img
+          data-aos="fade-up"
+          data-aos-duration="2000"
           className="rounded-xl"
           style={{ cursor: "pointer" }}
           src=" https://kit.envalabdemos.com/peat/wp-content/uploads/2023/09/pet-giving-paw-to-young-woman-2021-09-24-04-00-07-utc.jpg  "
